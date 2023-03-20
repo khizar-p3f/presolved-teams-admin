ee5fa4a4b1d94c6923bd8dc6532febdaefb415c8
Khizar <khizaras@gmail.com>
2023-03-21 01:44:24 +0530

    beautifiations

--- src/admin/pages/index.js
+++ src/admin/pages/index.js
@@ -1,5 +1,5 @@
-import { Avatar, Breadcrumb, Card, Col, Result, Row, Space, Typography } from 'antd'
-import { QuestionOutlined, UserOutlined, SettingOutlined, LogoutOutlined } from '@ant-design/icons';
+import { Alert, Avatar, Breadcrumb, Button, Card, Col, Empty, Result, Row, Space, Typography } from 'antd'
+import { PhoneOutlined, UserOutlined, SettingOutlined, LogoutOutlined } from '@ant-design/icons';
 import React, { useEffect, useState } from 'react'
 import MicrosoftLogin from "react-microsoft-login";
 import './index.less'
@@ -40,12 +40,12 @@ const AdminMainPage = (props) => {
                         <Card bordered={false} >
 
                             <Card.Meta title={<Space>
-                                <Typography.Title level={3}>Hi {client?.name}  - {client.company}</Typography.Title>
+                                <Typography.Title level={4}>Hi {client?.name}  - {client.company}</Typography.Title>
                             </Space>}
                             />
                             <Card.Meta description={
                                 <Space>
-                                    <Typography.Title level={4} type="secondary">Welcome to presolved dashboard for MS Teams integration</Typography.Title>
+                                    <Typography.Title level={5} type="secondary">Welcome to presolved dashboard for MS Teams integration</Typography.Title>
                                 </Space>
                             } />
                         </Card>
@@ -55,71 +55,133 @@ const AdminMainPage = (props) => {
 
 
                 <Row className='statistic-container' gutter={[16, 16]}>
-                    <Col span={6}>
-                        <Card  >
-                            <Card.Meta title={<Space>
-                                <Avatar size={64} icon={<UserOutlined />} />
-                                <Typography.Title level={3}>Users</Typography.Title>
-                            </Space>}
-                            />
-                            <Card.Meta description={
-                                <Space>
-                                    <Typography.Title level={4} type="secondary">Number of users using your app</Typography.Title>
-                                    <Typography.Title level={4}>100</Typography.Title>
-                                </Space>
-                            } />
-                        </Card>
-                    </Col>
-                    <Col span={6}>
-                        <Card  >
-                            <Card.Meta title={<Space>
-                                <Avatar size={64} icon={<QuestionOutlined />} />
-                                <Typography.Title level={3}>Tickets</Typography.Title>
-                            </Space>}
-                            />
-                            <Card.Meta description={
-                                <Space>
-                                    <Typography.Title level={4} type="secondary">Number of tickets created</Typography.Title>
-                                    <Typography.Title level={4}>100</Typography.Title>
-                                </Space>
-                            } />
-                        </Card>
-                    </Col>
-                    <Col span={6}>
-                        <Card  >
-                            <Card.Meta title={<Space>
-                                <Avatar size={64} icon={<UserOutlined />} />
-                                <Typography.Title level={3}>Users</Typography.Title>
-                            </Space>}
-                            />
-                            <Card.Meta description={
-                                <Space>
-                                    <Typography.Title level={4} type="secondary">Number of users using your app</Typography.Title>
-                                    <Typography.Title level={4}>100</Typography.Title>
-                                </Space>
-                            } />
-                        </Card>
-                    </Col>
-                    <Col span={6}>
-                        <Card  >
-                            <Card.Meta title={<Space>
-                                <Avatar size={64} icon={<UserOutlined />} />
-                                <Typography.Title level={3}>Users</Typography.Title>
-                            </Space>}
-                            />
-                            <Card.Meta description={
-                                <Space>
-                                    <Typography.Title level={4} type="secondary">Number of users using your app</Typography.Title>
-                                    <Typography.Title level={4}>100</Typography.Title>
-                                </Space>
-                            } />
-                        </Card>
-                    </Col>
+                    <Col span={24}>
+                        <Card bordered>
 
-
-
+                            <Card.Grid>
+                                <Card.Meta className='card-meta'
+                                    avatar={<Avatar size={42} icon={<PhoneOutlined />} />}
+                                    title={<Typography.Title level={3}>Monthly Statistics</Typography.Title>}
+                                />
+                                <div className='stats-container'>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text  >Calls</Typography.Text>
+                                            <Typography.Text strong >200</Typography.Text>
+                                        </Space>
+                                    </div>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text >Minutes</Typography.Text>
+                                            <Typography.Text strong >1565.00</Typography.Text>
+                                        </Space>
+                                    </div>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text >Failed</Typography.Text>
+                                            <Typography.Text strong >7</Typography.Text>
+                                        </Space>
+                                    </div>
+                                </div>
+                            </Card.Grid>
+                            <Card.Grid>
+                                <Card.Meta className='card-meta'
+                                    avatar={<Avatar size={42} icon={<PhoneOutlined />} />}
+                                    title={<Typography.Title level={3}>Weekly Statistics</Typography.Title>}
+                                />
+                                <div className='stats-container'>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text  >Calls</Typography.Text>
+                                            <Typography.Text strong >200</Typography.Text>
+                                        </Space>
+                                    </div>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text >Minutes</Typography.Text>
+                                            <Typography.Text strong >1565.00</Typography.Text>
+                                        </Space>
+                                    </div>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text >Failed</Typography.Text>
+                                            <Typography.Text strong >7</Typography.Text>
+                                        </Space>
+                                    </div>
+                                </div>
+                            </Card.Grid>
+                            <Card.Grid>
+                                <Card.Meta className='card-meta'
+                                    avatar={<Avatar size={42} icon={<PhoneOutlined />} />}
+                                    title={<Typography.Title level={3}>Today Statistics</Typography.Title>}
+                                />
+                                <div className='stats-container'>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text  >Calls</Typography.Text>
+                                            <Typography.Text strong >200</Typography.Text>
+                                        </Space>
+                                    </div>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text >Minutes</Typography.Text>
+                                            <Typography.Text strong >1565.00</Typography.Text>
+                                        </Space>
+                                    </div>
+                                    <div className='stats-items'>
+                                        <Space direction='vertical' size={10}>
+                                            <Typography.Text >Failed</Typography.Text>
+                                            <Typography.Text strong >7</Typography.Text>
+                                        </Space>
+                                    </div>
+                                </div>
+                            </Card.Grid>
+                        </Card>
+                    </Col>
                 </Row>
 
+                <Row className='calls-report-container' gutter={[16, 16]}>
+                    <Col span={12}>
+                        <Card title="Recent Calls" extra={[<Button icon={<SettingOutlined/>}>Settings</Button> ]}>
+                            <Empty>
+                                <Typography.Text>No recent calls</Typography.Text>
+                            </Empty>
+                            
+                        </Card>
+                    </Col>
+                    <Col span={12}>
+                        <Card title="Group Calls" extra={[<Button icon={<SettingOutlined/>}>Settings</Button> ]}>
+                            <Empty>
+                                <Typography.Text>No Group calls</Typography.Text>
+                            </Empty>
+                            
+                        </Card>
+                    </Col>
+                </Row>
+
+                <Row className='alerts-container' gutter={[16, 16]}>
+                    <Col span={24}>
+                        <Alert banner
+                        closable
+                        message="You haven't Signing with Microsoft Teams yet. Please click the button below to sign in"
+                        description={<Button type="primary" icon={<UserOutlined />}>Sign in with Microsoft Teams</Button>}
+                        type="warning"
+                        showIcon
+                        />
+                    </Col>
+
+                    <Col span={24}>
+                        <Alert banner
+                        role='alert'
+                        closable
+                        message="You need to get a consent from Microsoft Teams to access your data. Please click the button below to sign in"
+                        description={<Button type="primary" icon={<UserOutlined />}>Get consent</Button>}
+                        type="warning"
+                        showIcon
+                        />
+                    </Col>
+                    
+                </Row>
             </div>
         </div>
     )
