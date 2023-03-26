8aa8d8a457d1b7afc39ad2d68d70f743e212f99b
sivashanmugam Thangavel <sivashanmugam@gmail.com>
2023-03-26 10:52:49 -0700

    Added backend changes for Teams Auth

---
+++ amplify/backend/function/MSTeamsClientsAPI/src/app.js
@@ -0,0 +1,133 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["CallManagerAPIKey"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
+const MSTeams = require("./teams");
+const msteams = new MSTeams();
+
+// declare a new express app
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+/**********************
+ * Example get method *
+ **********************/
+
+app.get("/teams", function (req, res) {
+  // Add your code here
+  res.json({ success: "get call succeed!", url: req.url });
+});
+
+app.get("/teams/users", async function (req, res) {
+  // Add your code here
+  //Authenticate the call
+  //authenticateAllCalls(req, res, next);
+  //Validate the user using the authtoken
+  let authToken = req.headers.authorization;
+  //Remove Bearer from the token
+  authToken = authToken.replace("Bearer ", "");
+  //console.log("Auth Token is ", authToken);
+  //let userDetails = await msteams.validateAuthToken(authToken);
+  let validatedTokenResult = await msteams.validateIdToken(
+    authToken,
+    req.query.tenantId
+  );
+  console.log("Validated Token Result is ", validatedTokenResult);
+  // console.log("User Details are ", userDetails);
+  if (validatedTokenResult) {
+    console.log("Call is from a authenticated client from MS Teams");
+    let usersList = await msteams.getUsers(
+      req.query.tenantId,
+      req.query.displayName
+    );
+    res.json({ success: "get users call succeed!", users: usersList });
+  } else {
+    //Send 401
+    res.status(401).send("Unauthorized");
+  }
+});
+
+app.get("/teams/*", function (req, res) {
+  // Add your code here
+  res.json({ success: "get call succeed!", url: req.url });
+});
+
+/****************************
+ * Example post method *
+ ****************************/
+
+app.post("/teams", function (req, res) {
+  // Add your code here
+  res.json({ success: "post call succeed!", url: req.url, body: req.body });
+});
+
+app.post("/teams/*", function (req, res) {
+  // Add your code here
+  res.json({ success: "post call succeed!", url: req.url, body: req.body });
+});
+
+/****************************
+ * Example put method *
+ ****************************/
+
+app.put("/teams", function (req, res) {
+  // Add your code here
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
+});
+
+app.put("/teams/*", function (req, res) {
+  // Add your code here
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
+});
+
+/****************************
+ * Example delete method *
+ ****************************/
+
+app.delete("/teams", function (req, res) {
+  // Add your code here
+  res.json({ success: "delete call succeed!", url: req.url });
+});
+
+app.delete("/teams/*", function (req, res) {
+  // Add your code here
+  res.json({ success: "delete call succeed!", url: req.url });
+});
+
+app.listen(3000, function () {
+  console.log("App started");
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app;
---
+++ amplify/backend/function/MSTeamsClientsAPI/src/teams.js
@@ -0,0 +1,56 @@
+const axios = require("axios");
+const validate = require("validate-azure-ad-token").default;
+
+class MSTeams {
+  constructor() {
+    this.apiURL = process.env.CallManagerURL;
+  }
+
+  async getUsers(tenantId, displayName) {
+    const url = `${this.apiURL}/api/users?tenantId=${tenantId}&displayName=${displayName}`;
+    const response = await axios.get(url);
+    return response.data;
+  }
+
+  /* Validate the MS Graph Idtoken */
+  async validateIdToken(idToken, tenantId) {
+    try {
+      const decodedToken = await validate(idToken, {
+        tenantId: tenantId,
+        audience: "00000003-0000-0000-c000-000000000000",
+        applicationId: "32c21b01-f645-4433-8726-456c9f46958e",
+        scopes: ["User.Read"], // for example ["User.Read"]
+      });
+
+      //console.log("Decoded Token is ", decodedToken);
+      //let expiryTime = decodedToken.payload.exp;
+      //Convert Expiry time to Date Time Format
+      //let expiryDate = new Date(0);
+      //expiryDate.setUTCSeconds(expiryTime);
+      //console.log("Expiry Date is ", expiryDate);
+      return true;
+    } catch (error) {
+      // ALL ERRORS GONNA SHOW HERE AS A STRING VALUE
+      console.log("Error validating the token ", error);
+      return false;
+    }
+  }
+
+  /* Check if the token is valid by calling MS Graph API */
+  async validateAuthToken(token) {
+    try {
+      const url = `https://graph.microsoft.com/v1.0/me`;
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: token,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log("Error validating the token ", error);
+      return null;
+    }
+  }
+}
+
+module.exports = MSTeams;
