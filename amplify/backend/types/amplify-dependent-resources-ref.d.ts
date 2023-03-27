e7f88535d9a812442c5d670d506cd6079da180b7
sivashanmugam Thangavel <sivashanmugam@gmail.com>
2023-03-27 14:30:50 -0700

    Added hosting

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -48,33 +48,17 @@ export type AmplifyDependentResourcesAttributes = {
             "ApiName": "string",
             "ApiId": "string"
         }
-    }
-  },
-  "auth": {
-    "presolvedteamsadmine76efdae": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
     },
-    "userPoolGroups": {
-      "p3fAdminGroupRole": "string",
-      "p3fSupportGroupRole": "string",
-      "tenantAdminGroupRole": "string",
-      "tenantSupervisorGroupRole": "string",
-      "tenantUserGroupRole": "string"
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string",
+            "CloudFrontDistributionID": "string",
+            "CloudFrontDomainName": "string",
+            "CloudFrontSecureURL": "string",
+            "CloudFrontOriginAccessIdentity": "string"
+        }
     }
-  },
-  "function": {
-    "AdminQueriescecc2b38": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
 }
